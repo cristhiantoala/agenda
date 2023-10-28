408d0775001cd0b027c5ff7166f5c952d69f7f05
Cristhian Maldonado <crismaldonado96@hotmail.com>
2023-10-28 17:47:01 -0500

    Restauración de repositorio

--- src/main/java/dagatecompany/agenda/AgendaApplication.java
+++ src/main/java/dagatecompany/agenda/AgendaApplication.java
@@ -2,11 +2,8 @@ package dagatecompany.agenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-=======
 import org.springframework.context.annotation.Bean;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
->>>>>>> 2745562bca63603c9b3a057becc04471228b6d63
 
 @SpringBootApplication
 public class AgendaApplication {
@@ -14,12 +11,8 @@ public class AgendaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApplication.class, args);
 	}
-<<<<<<< HEAD
-
-=======
 	@Bean
 	public SpringDataDialect springDataDialect() {
 		return new SpringDataDialect();
 	}
->>>>>>> 2745562bca63603c9b3a057becc04471228b6d63
 }
--- src/main/java/dagatecompany/agenda/controller/ContactoController.java
+++ src/main/java/dagatecompany/agenda/controller/ContactoController.java
@@ -3,13 +3,10 @@ package dagatecompany.agenda.controller;
 import dagatecompany.agenda.model.Contacto;
 import dagatecompany.agenda.repository.ContactoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-=======
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
->>>>>>> 2745562bca63603c9b3a057becc04471228b6d63
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,10 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.RequestParam;
->>>>>>> 2745562bca63603c9b3a057becc04471228b6d63
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.thymeleaf.model.IModel;
 
@@ -34,11 +28,6 @@ public class ContactoController {
     private ContactoRepository contactoRepository;
 
     @GetMapping
-<<<<<<< HEAD
-    String index(Model model) {
-        List<Contacto> contactos = contactoRepository.findAll();
-        model.addAttribute("contactos", contactos);
-=======
     String index(@PageableDefault(size = 4, sort = "fechaRegistro", direction = Sort.Direction.DESC)
                  Pageable pageable,
                  @RequestParam(required = false) String busqueda,
@@ -52,7 +41,6 @@ public class ContactoController {
             contactoPage=contactoRepository.findAll(pageable);
         }
         model.addAttribute("contactoPage", contactoPage);
->>>>>>> 2745562bca63603c9b3a057becc04471228b6d63
         return "index";
     }
 
@@ -104,10 +92,7 @@ public class ContactoController {
         ra.addFlashAttribute("msgExito", "El contacto se ha actualizado");
         return "redirect:/";
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> 2745562bca63603c9b3a057becc04471228b6d63
     @PostMapping("/{id}/eliminar")
     String eliminar(
             @PathVariable Integer id,
